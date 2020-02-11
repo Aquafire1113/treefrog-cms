beabd2838f88e8b7f72b67442854ca8782528064
Abbigail Merrill <cattleshowgirl97@yahoo.com>
2020-02-11 12:48:53 -0500

    Added modal function

--- public/app/app.js
+++ public/app/app.js
@@ -2,8 +2,7 @@ function initButtons(){
     $(".get-started").click(function(e){
         $("#home div").removeClass('active');
         $("#addNav div").addClass('active');
-        $('.text-wrapper').html(SERVICE.getGetStartedContent);
-        $('.btn-holder').html(SERVICE.getCreateNavButton);
+        displayModal();
     })
 
     $("#home").click(function(e){
@@ -11,6 +10,17 @@ function initButtons(){
         $("#home div").addClass('active');
         $('.text-wrapper').html(SERVICE.getHomeContent);
         $('.btn-holder').html(SERVICE.getHomeStartButton);
+    })
+}
+
+function displayModal(){
+
+    $('.text-wrapper').html(SERVICE.getGetStartedContent);
+    $('.btn-holder').html(SERVICE.getCreateNavButton);
+
+    $(".nav-modal").click(function(e){
+        console.log('click');
+        $(".modal").css("display", "flex");
     })
 }
 
--- public/app/service.js
+++ public/app/service.js
@@ -5,7 +5,11 @@ var SERVICE = (function(){
         <h1>Treefrog CMS</h1>
         <p>This is the screen where you will create your navigation and page content</p>
         <p>First, you will need to create a main navigation. Once you have created a main navigation you can create a sub-navigation if you would like to.</p>
-        <p>Once you create either a nav or sub-nav, a text editor will pop up and you will be allowed to create your page content.</p>`;
+        <p>Once you create either a nav or sub-nav, a text editor will pop up and you will be allowed to create your page content.</p>
+        <div class="modal">
+            <div class="alert-box"></div>
+        </div>  
+        `;
 
         return contentString;
     }
@@ -13,8 +17,8 @@ var SERVICE = (function(){
     var _getCreateNavButton = function(){
         let buttonString = `
         
-        <span class="btn btn-dark ">Create Main Nav</span>
-        <span class="btn btn-dark ">Create Sub-Nav</span>
+        <span class="btn btn-dark nav-modal">Create Main Nav</span>
+        <span class="btn btn-dark">Create Sub-Nav</span>
         
         `;
 

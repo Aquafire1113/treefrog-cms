24c5d930b4fd69f455257a2121183590c218d373
Abbigail Merrill <cattleshowgirl97@yahoo.com>
2020-02-20 19:41:53 -0500

    Finished Assignment for homework 4

--- public/app/app.js
+++ public/app/app.js
@@ -1,29 +1,87 @@
 function initButtons(){
-    $(".get-started").click(function(e){
-        $("#home div").removeClass('active');
-        $("#addNav div").addClass('active');
-        displayModal();
-    })
-
     $("#home").click(function(e){
         $("#addNav div").removeClass('active');
         $("#home div").addClass('active');
         $('.text-wrapper').html(SERVICE.getHomeContent);
         $('.btn-holder').html(SERVICE.getHomeStartButton);
+        addGetStartedListener();
+        $(".nav-modal").off();
+    })
+}
+
+function addGetStartedListener(){
+    $(".get-started").click(function(e){
+        $("#home div").removeClass('active');
+        $("#addNav div").addClass('active');
+        $('.text-wrapper').html(SERVICE.getGetStartedContent);
+        $('.btn-holder').html(SERVICE.getCreateNavButton);
+        $(".alert-box").html(SERVICE.modalContent);
+        displayModal();
+        $(".get-started").off(event);
     })
 }
 
 function displayModal(){
 
-    $('.text-wrapper').html(SERVICE.getGetStartedContent);
-    $('.btn-holder').html(SERVICE.getCreateNavButton);
+    var finalText="";
 
     $(".nav-modal").click(function(e){
-        console.log('click');
         $(".modal").css("display", "flex");
     })
+
+    $(".cancel").click(function(e){
+        $(".modal").css("display", "none");
+    })
+    
+    $(".clickBox").click(function(e){
+        $(".modal").css("display", "none");
+    })
+
+    $( ".linkInput" ).keyup(function() {
+        finalText = $( this ).val();
+    }).keyup();
+
+    $(".createLink").click(function(){
+        if(finalText.trim().length !== 0){
+            $(".modal").css("display", "none");
+            $(".mainNavModal").css("display", "flex");
+            $(".navName").html(finalText);
+        }
+    });
+
+    var toolbarOptions = [
+
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                
+
+     ]
+
+    var quill = new Quill('#editor', {
+        modules :{
+            toolbar: toolbarOptions
+        },
+        theme: 'snow'
+      });
 }
+
+
 
 $(document).ready(function(){
     initButtons();
+    addGetStartedListener();
 });
--- public/app/service.js
+++ public/app/service.js
@@ -6,9 +6,24 @@ var SERVICE = (function(){
         <p>This is the screen where you will create your navigation and page content</p>
         <p>First, you will need to create a main navigation. Once you have created a main navigation you can create a sub-navigation if you would like to.</p>
         <p>Once you create either a nav or sub-nav, a text editor will pop up and you will be allowed to create your page content.</p>
+        <div class='mainNavModal'>
+        <h1>Treefrog CMS</h1>
+        <p>Now that you have your navigation set you can now create your content. Below you will see your navigation name and a text editor. Create your content in the text editor and then click on "Save page Info". Once you have done that click on the "PREVIEW SITE" link to see what your web page looks like.</p>
+
+        <b><span> Nav > <span class="navName"></span></span></b>
+
+        <div id="editor">
+
+        </div>
+
+        <span class='btn btn-dark'>Save Page Info</span>
+        
+        </div> 
         <div class="modal">
+            <div class="clickBox"></div>
             <div class="alert-box"></div>
-        </div>  
+        </div> 
+        
         `;
 
         return contentString;
@@ -59,11 +74,27 @@ var SERVICE = (function(){
         return startButtonString;
     }
 
+    var _modalContent = function(){
+        let modalString = `
+        <h1>Use this box to create Navigation Links</h1>
+        <p>You can create main navigation and sub navigation. To create a sub-navigation you will need to first select a main nav and then create the sub-nav.</p>
+        <p>Using the text box below, enter the name of your main navigation.</p>
+        <input class='linkInput' type="text" value=""/>
+        <div>
+        <span id="createMainNav" class="btn btn-light get-started createLink">Create Main Nav</span>
+
+        <span class="btn btn-light cancel">Cancel</span>
+        </div>
+        `;
+        return modalString;
+    }
+
     return {
         getGetStartedContent: _getGetStartedContent,
         getCreateNavButton: _getCreateNavButton,
         getHomeContent: _getHomeContent,
-        getHomeStartButton:_getHomeStartButton
+        getHomeStartButton:_getHomeStartButton,
+        modalContent: _modalContent
     }
 
 })();
